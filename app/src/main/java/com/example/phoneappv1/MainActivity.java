3fa0a0c7207271ed9453b9bbcbded1d34b6d6304
Avnith <avnith.v@gmail.com>
2022-10-22 13:42:31 -0400

    deleted some files

--- app/src/main/java/com/example/phoneappv1/MainActivity.java
+++ app/src/main/java/com/example/phoneappv1/MainActivity.java
@@ -24,18 +24,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void Science(View v){
-        setContentView(R.layout.science_main);
-    }
 
-    public void Biology(View v){
-        Button b = (Button)v;
-        String text = b.getText().toString();
-        Class c = new Class(text);
-        setContentView(R.layout.biology_main);
-    }
-
-    public void exit(){
-        setContentView(R.layout.activity_main);
-    }
 }
